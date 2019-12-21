06b4ad8e26bfca179579c6261d294366174adfdf
Pulkit gera <pulkithifi@yahoo.com>
2019-12-21 12:33:34 +0530

    surface normal added

--- weekend/ray1.cpp
+++ weekend/ray1.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
 #include <stdlib.h>
+#include <math.h>
 // #include "vec3.h"
 #include "ray.h"
 using namespace std;
+// Now we compute surface normals and intersect rays with them
+float hit_sphere(const vec3& center,float radius,const ray& r){
+    vec3 oc = r.origin()-center;
+    float a = dot(r.direction(),r.direction());
+    float b = 2*dot(oc,r.direction());
+    float c = dot(oc,oc)-radius*radius;
+    float discrimiant = b*b-4*a*c;
+    // cout<<discrimiant<<" D"<<endl;
+    if(discrimiant<0){
+        return -1.0;
+    }
+    else{
+        return (-b - sqrt(discrimiant))/(2*a);
+    }
+}
+
+
+
 // check if a sphere intersects with a given ray
 bool hitsphere(const vec3& center, float radius, const ray& r){
     vec3 oc = r.origin()-center;
@@ -17,22 +36,45 @@ bool hitsphere(const vec3& center, float radius, const ray& r){
 }
 
 // linear interpolation between white and blue based on position of y
+// vec3 color(ray &r){
+//     if (hitsphere(vec3(0,0,-1),0.25,r)){
+//         return vec3(1,1,0);
+//     }
+//     if (hitsphere(vec3(1,0,-1),0.5,r)){
+//         return vec3(1,0,1);
+//     }
+//     if (hitsphere(vec3(0,1,-1),0.5,r)){
+//         return vec3(0,1,1);
+//     }
+//     if (hitsphere(vec3(1,1,-2),0.25,r)){
+//         return vec3(0,0.5,1);
+//     }
+//     vec3 unit = unit_vector(r.direction());
+//     float t= 0.5*(unit.y()+1.0);
+//     return (1-t)*vec3(1,1,1)+t*vec3(0.5,0.7,1);
+// }
 vec3 color(ray &r){
-    if (hitsphere(vec3(0,0,-1),0.25,r)){
-        return vec3(1,1,0);
+    // temporary sphere
+    vec3 sphere = vec3(-1,0,-1);
+    vec3 sphere1 = vec3(1,0,-1);
+    float t = hit_sphere(sphere,0.75,r);
+    float t2 = hit_sphere(sphere1,0.75,r);
+    // cout<<t<<" value"<<"\n";
+    if(t>0){
+        // Surface normal, we interesect a ray with that and provide a color
+        // std::cout<<"Hello";
+        vec3 N = unit_vector(r.point_at_parameter(t)-sphere);
+        return 0.5*vec3(N.x()+1,N.y()+1,N.z()+1);
     }
-    if (hitsphere(vec3(1,0,-1),0.5,r)){
-        return vec3(1,0,1);
-    }
-    if (hitsphere(vec3(0,1,-1),0.5,r)){
-        return vec3(0,1,1);
-    }
-    if (hitsphere(vec3(1,1,-2),0.25,r)){
-        return vec3(0,0.5,1);
+    if(t2>0){
+        // Surface normal, we interesect a ray with that and provide a color
+        // std::cout<<"Hello";
+        vec3 N = unit_vector(r.point_at_parameter(t2)-sphere1);
+        return 0.5*vec3(N.x()+1,N.y()+1,N.z()+1);
     }
     vec3 unit = unit_vector(r.direction());
-    float t= 0.5*(unit.y()+1.0);
-    return (1-t)*vec3(1,1,1)+t*vec3(0.5,0.7,1);
+    float t1= 0.5*(unit.y()+1.0);
+    return (1-t1)*vec3(1,1,1)+t1*vec3(0.5,0.7,1);
 }
 int main()
 {
